1045f85530a4fe65fe31e7cb3cd2772e336f7827
Muhammad Lahin <muhammad.lahin@gmail.com>
2017-10-19 19:23:02 +0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,10 @@ import { trigger, transition, group, query, style, animate } from '@angular/anim
             style({ height: '!' }),
             query(':enter', style({ transform: 'translateX(100%)' })),
             query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
-            // animate the leave page away
             group([
                 query(':leave', [
                     animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(-100%)' })),
                 ]),
-                // and now reveal the enter
                 query(':enter', animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)' }))),
             ]),
         ]),
@@ -24,12 +22,10 @@ import { trigger, transition, group, query, style, animate } from '@angular/anim
             style({ height: '!' }),
             query(':enter', style({ transform: 'translateX(-100%)' })),
             query(':enter, :leave', style({ position: 'absolute', top: 0, left: 0, right: 0 })),
-            // animate the leave page away
             group([
                 query(':leave', [
                     animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(100%)' })),
                 ]),
-                // and now reveal the enter
                 query(':enter', animate('0.3s cubic-bezier(.35,0,.25,1)', style({ transform: 'translateX(0)' }))),
             ]),
         ]),
